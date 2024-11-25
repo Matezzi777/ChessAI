d2ee5f6a6ec2be9f789900f6bfa767001e2b39d9
Matezzi777 <matezzi777@gmail.com>
2024-11-25 15:20:26 +0100

    UI to rework

--- main.c
+++ main.c
@@ -10,11 +10,13 @@ int	main(void)
 	mlx = mlx_init();
 	window = mlx_new_window(mlx, 900, 900, "ChessAI");
 	board = board_init();
+	if (!board)
+		return (0);
 	if (!window)
 		return (0);
 	img.img = mlx_new_image(mlx, 900, 900);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	draw_board(mlx, window, &img, board);
+	draw_board(&img);
 	mlx_put_image_to_window(mlx, window, img.img, 0, 0);
 	mlx_loop(mlx);
 }
